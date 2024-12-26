dfb8ed22d49032efcb574a9ca1f9fb38b37f67ad
sergei.kosarev+qa@channelfactory.com <sergei.kosarev+qa@channelfactory.com>
2024-12-25 16:19:17 -0800

    test

--- cypress.config.js
+++ cypress.config.js
@@ -3,21 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      console.log('process.env.ENV_TYPE:', process.env.ENV_TYPE); // Check environment variable in the Node process
-      console.log('config.env.ENV_TYPE:', config.env.ENV_TYPE);   // Check Cypress-provided value
-      
-      const envType = config.env.ENV_TYPE || process.env.ENV_TYPE || 'local';
-      console.log('Environment Type:', envType);                 // Confirm the effective environment type
-
-      if (envType === 'rc') {
-        config.baseUrl = 'https://google.com';
-      } else if (envType === 'prod') {
-        config.baseUrl = 'https://facebook.com';
-      } else {
-        config.baseUrl = 'https://localhost'; // Default fallback
-      }
-      
-      console.log('Base URL set to:', config.baseUrl);
+     
       return config;
     },
     failOnStatusCode: false,
--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -17,11 +17,10 @@ describe('example to-do app', () => {
     // so we must tell it to visit our website with the `cy.visit()` command.
     // Since we want to visit the same URL at the start of all our tests,
     // we include it in our beforeEach function so that it runs before each test
-    cy.visit('/')
+    cy.visit('https://example.cypress.io/commands/')
   })
 
-  it('displays two todo items by default', () => {
-    expect(true).to.equal(false);
+  it.only('displays two todo items by default', () => {
     // We use the `cy.get()` command to get all elements that match the selector.
     // Then, we use `should` to assert that there are two matched items,
     // which are the two default items.
