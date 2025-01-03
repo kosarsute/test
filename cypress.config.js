6363c1176fbf334621ef1a66f7c1fe2885483f4d
sergei.kosarev+qa@channelfactory.com <sergei.kosarev+qa@channelfactory.com>
2025-01-03 14:59:53 -0800

    test

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("cypress");
+const path = require("path");
 
 module.exports = defineConfig({
   e2e: {
@@ -14,10 +15,41 @@ module.exports = defineConfig({
         // Fallback for other environments or when ENV_TYPE is undefined
         config.baseUrl = "https://www.example.com";
       }
-      console.log("OUR URL:", config.baseUrl)
+      console.log("OUR URL:", config.baseUrl);
+
       // Return the updated config
       return config;
     },
     failOnStatusCode: false,
+    screenshotsFolder: process.env.TEAMCITY_BUILD_CHECKOUT_DIR
+      ? path.join(process.env.TEAMCITY_BUILD_CHECKOUT_DIR, "cypress/screenshots")
+      : "cypress/screenshots", // Dynamically set screenshots folder
+  },
+  reporter: "cypress-multi-reporters",
+  reporterOptions: {
+    reporterEnabled: "mochawesome, cypress-qase-reporter",
+    mochawesomeReporterOptions: {
+      reportDir: "cypress/reports/mochawesome",
+      overwrite: false,
+      html: true,
+      json: true,
+      charts: true,
+      reportFilename: `mochawesome_${Date.now()}`, // Unique filename
+    },
+    cypressQaseReporterReporterOptions: {
+      debug: false,
+      testops: {
+        api: {
+          token: "your-qase-api-token",
+        },
+        project: "YOUR_PROJECT_CODE",
+        uploadAttachments: true,
+        run: {
+          complete: true,
+          title: `Cypress Automated Run`,
+          id: process.env.QASE_RUN_ID,
+        },
+      },
+    },
   },
 });
